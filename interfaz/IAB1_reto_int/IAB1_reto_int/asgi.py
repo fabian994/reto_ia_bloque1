3ed5e73474f866c6a919ed05ec4c1dde928bfe50
fabian994 <a01367585@itesm.mx>
2023-09-05 12:25:43 -0600

    started django project

---
+++ interfaz/IAB1_reto_int/IAB1_reto_int/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for IAB1_reto_int project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'IAB1_reto_int.settings')
+
+application = get_asgi_application()
